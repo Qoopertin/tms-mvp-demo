9d7d5a13aab853941b9d4742dd3518eb7e33c3b9
TMS Developer <tms@example.com>
2025-12-23 18:30:51 +0100

    chore: enable debug mode temporarily

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name' => env('APP_NAME', 'Laravel'),
     'env' => env('APP_ENV', 'production'),
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
     'url' => env('APP_URL', 'http://localhost'),
     'timezone' => env('APP_TIMEZONE', 'UTC'),
     'locale' => env('APP_LOCALE', 'en'),
